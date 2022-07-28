a38fefe6f72cc4114ac1e8fa4f00942e8a489d8e
Dan <amorecbellum@gmail.com>
2022-07-27 20:43:58 -0500

    Added commands

--- paper/src/main/java/sh/okx/railswitch/RailSwitchPlugin.java
+++ paper/src/main/java/sh/okx/railswitch/RailSwitchPlugin.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import sh.okx.railswitch.switches.SwitchListener;
+import com.google.common.base.CharMatcher;
 
 import java.util.HashMap;
 import java.util.UUID;
@@ -21,6 +22,19 @@ public final class RailSwitchPlugin extends JavaPlugin implements Listener {
 
     public String getDestination(Player p) {
         return destinations.get(p.getUniqueId());
+    }
+    public boolean isValidDestination(String message) {
+        // Each destination must be fewer than 40 characters.
+        for (String dest : message.split(" ")) {
+            if (dest.length() > 40) {
+                return false;
+            }
+        }
+
+        return CharMatcher.inRange('0', '9')
+                .or(CharMatcher.inRange('a', 'z'))
+                .or(CharMatcher.inRange('A', 'Z'))
+                .or(CharMatcher.anyOf("!\"#$%&'()*+,-./;:<=>?@[]\\^_`{|}~ ")).matchesAllOf(message);
     }
 
     @Override
